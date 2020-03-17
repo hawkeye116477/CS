d616891ba4fec983360450bb66c1055a2ca39427
hawkeye116477 <hawkeye116477@gmail.com>
2020-03-17 22:02:16 +0100

    Update build script.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+XPI_FILE=CS
+
+path=$(dirname "$0")
+
+cd $path/src
+
+echo "- Removing old xpi file"
+if [ -f "$XPI_FILE.xpi" ]; then
+    rm $XPI_FILE.xpi
+fi
+
+echo "- Creating xpi file"
+zip -r $XPI_FILE.xpi
